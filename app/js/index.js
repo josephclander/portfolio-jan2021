9e8a0f749d9bd47f2585a3df64a93449d70a2b85
Joseph Lander <josephclander@gmail.com>
2021-01-11 15:33:28 +0000

    Add markup and js for navigation button

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,19 @@
-console.log('The js file is connected!');
+const navModal = document.querySelector('.nav-modal');
+const menu = document.querySelector('.menu');
+const navButton = document.querySelector('.burger-menu');
+
+const showNavigation = () => {
+  navModal.style.transform = 'scale(6)';
+  menu.style.transform = 'translate(-50%, -50%)';
+  menu.style.opacity = '1';
+};
+
+const hideNavigation = () => {
+  menu.style.opacity = '0';
+  menu.style.transform = 'translate(50%, -50%)';
+  navModal.style.transform = 'scale(0)';
+};
+
+navButton.addEventListener('click', showNavigation);
+navModal.addEventListener('click', hideNavigation);
+menu.addEventListener('click', hideNavigation);
