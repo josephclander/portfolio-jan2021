84deca0743fa950440679af8c80062a2bd332909
Joseph Lander <josephclander@gmail.com>
2021-01-20 16:34:28 +0000

    Add scroll effects

--- app/js/index.js
+++ app/js/index.js
@@ -1,3 +1,4 @@
+// Modal Navigation
 const navModal = document.querySelector('.nav-modal');
 const menu = document.querySelector('.menu');
 const navButton = document.querySelector('.burger-menu');
@@ -17,3 +18,36 @@ const hideNavigation = () => {
 navButton.addEventListener('click', showNavigation);
 navModal.addEventListener('click', hideNavigation);
 menu.addEventListener('click', hideNavigation);
+
+// Page Scroll Effects
+const redContainer = document.querySelector('.red-container');
+const yellowContainer = document.querySelector('.yellow-container');
+const arrow = document.querySelector('.arrow');
+
+const scrollEffects = () => {
+  if (
+    document.body.scrollTop > 50 ||
+    window.pageYOffset > 50 ||
+    document.documentElement.scrollTop > 50
+  ) {
+    redContainer.style.transform = 'scale(0.7)';
+    yellowContainer.style.transform = 'scaleX(0.8) translateX(-20px)';
+    arrow.style.transform = 'scaleY(2) translateY(20px)';
+  } else {
+    redContainer.style.transform = 'scale(1)';
+    yellowContainer.style.transform = 'scaleX(1) translateX(0)';
+    arrow.style.transform = 'scaleY(1) translateY(0)';
+  }
+};
+
+// window.onscroll = function () {
+//   scrollEffects();
+// };
+
+document.addEventListener(
+  'scroll',
+  () => {
+    scrollEffects();
+  },
+  { capture: false, passive: true }
+);
